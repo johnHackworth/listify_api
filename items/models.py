8d469a14b71084c4227f7da73797f7e2785f68d6
Javi Alvarez <jalvarez@youzee.com>
2012-03-23 19:58:18 +0100

    items app start

---
+++ items/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+import json
+
+class Item(models.Model):
+	name = models.CharField(max_length=255)
+	url = models.CharField(max_length=255)
+	user_id = models.IntegerField(max_length=11)
+	category = models.IntegerField(max_length=5)
+	list_id = models.IntegerField(max_length=9)
+	image_url = models.CharField(max_length=255)
+	text = models.CharField(max_length=1024)
+	price = models.FloatField(max_length=255)
+	currency = models.IntegerField(max_length=3)
+	author = models.CharField(max_length=255)
+	date = models.TimeField()
+	image_id = models.IntegerField(max_length=9)
+	state = models.IntegerField(max_length=11)
+	screencap = models.CharField(max_length=255)     

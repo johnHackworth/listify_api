2de6bf5b364293ecc9616c3ed8bee6d0231ff3ef
johnHackworth <javieralvarezlop@gmail.com>
2012-04-06 18:27:06 +0200

    deletion of old files

--- users/services/user_service.py
+++
@@ -1,9 +0,0 @@
-from users.models import User
-
-class User_service():
-	def findUser(self, filter):
-		users = User.objects.filter(filter);
-		if len(users) > 0:
-			return users[0]
-		else:
-			return None
